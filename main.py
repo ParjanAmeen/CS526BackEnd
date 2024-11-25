e7b299aaa0c5633c18052b96b655d676813cf794
ashaikh45 <90731194+ashaikh45@users.noreply.github.com>
2024-11-24 22:17:34 -0500

    updated process frames method

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import cv2
+import pickle
+import pandas as pd
+from ultralytics import YOLO
+import cvzone
+import json
+
+# Constants and file paths
+coco_classes_file = "coco.txt"
+yolo_model = 'yolov8s.pt'
+
+# Load class names for object detection
+with open(coco_classes_file, "r") as file:
+    class_list = file.read().split("\n")
+
+# Initialize YOLO model for object detection
+model = YOLO(yolo_model)
+
 
 def process_frame(frame):
     # Predict objects in the frame using the YOLO model
